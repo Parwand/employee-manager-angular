af407e79a2c6d2ddce7cb026ba7c04f9dc88bb4d
Parwand Alsino <parwand.alsino@hhu.de>
2022-09-18 01:23:12 +0200

    handled the user not found exception

--- src/main/java/net/parwand/employeemanagerangular/exception/UserNotFoundException.java
+++ src/main/java/net/parwand/employeemanagerangular/exception/UserNotFoundException.java
@@ -1,2 +1,10 @@
-package net.parwand.employeemanagerangular;public class UserNotFoundException {
+package net.parwand.employeemanagerangular.exception;
+
+import org.springframework.data.crossstore.ChangeSetPersister;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
